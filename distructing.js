33c4d2649fabd22706ca163cdd8baffc9f00fceb
DIVYAV-2004 <divyavallabhapuramd1@gmail.com>
2025-10-21 21:32:53 +0530

    Add files via upload

---
+++ distructing.js
@@ -0,0 +1,18 @@
+//Destructing
+//1.Array Distructing
+var festival=['new year','christmas','Sankranthi','diwali']
+var [one,two,three,four]=festival;
+console.log(one)
+console.log(two)
+console.log(three)
+console.log(four)
+
+//2.Object Distructing
+var person = {name:"divya",age:22,place:"Guntur"}
+var {name:a,age:b,place:c} =person;
+console.log(a)
+console.log(b)
+console.log(c)
+
+
+//take input from user and store it salary 
