9129d2f148107b975ab34adeee3710f9db8e43b5
DIVYAV-2004 <divyavallabhapuramd1@gmail.com>
2025-10-21 21:36:25 +0530

    Add files via upload

---
+++ task.js
@@ -0,0 +1,5 @@
+var name=prompt("Enter employee Name : ");
+document.write("Employee Name : "+name+"<br>")
+var salary=prompt("enter employee Salary : ");
+document.write("Employe Salary : ")
+salary>50000?document.write("Employe Salary is greater"):document.write("employee salary is less")
