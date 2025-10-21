6aad441eb4fa646af457fef72bec76bc8802cf5b
DIVYAV-2004 <divyavallabhapuramd1@gmail.com>
2025-10-21 21:32:39 +0530

    Add files via upload

---
+++ class1.js
@@ -0,0 +1,20 @@
+//javascript
+//Definition: JavaScript is a high-level, interpreted programming language
+// used to make web pages interactive and dynamic. 
+// It is one of the core technologies of the web, 
+// along with HTML and CSS.
+
+//javascript variables
+//def:-A Variable in JavaScript is a named container used to store date or value. 
+// Which is used to perform operations like Reusing, Manipulating.
+//Rules to write Variable names:
+//1.Variable name must be case-sensitive
+// 2. Cannot start with a number.
+// 3. Can start with letters (a-z, A-Z), underscore (_), or dollar sign ($).
+// 4. Numbers can be used in the middle or end.
+// Examples:
+// let name; 
+// let name; 
+// let $name; 
+
+//
