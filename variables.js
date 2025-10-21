de0c3f3e7861fcea12b59a2bfe42cb1ed6bfbadd
DIVYAV-2004 <divyavallabhapuramd1@gmail.com>
2025-10-21 21:33:41 +0530

    Add files via upload

---
+++ variables.js
@@ -0,0 +1,7 @@
+let studentName = "Divya";
+let totalMarks = 75;
+let isPassed = totalMarks>40;
+
+document.write("Student Name: "+ studentName + "<br>");
+document.write("Total Marks: " + totalMarks + "<br>");
+document.write("Passed: " + isPassed + "<br>");
