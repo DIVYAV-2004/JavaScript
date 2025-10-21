0cff526cc7d1209ca606cc733a41a4286c70fb93
DIVYAV-2004 <divyavallabhapuramd1@gmail.com>
2025-10-21 21:38:54 +0530

    Add files via upload

---
+++ js2.js
@@ -0,0 +1,16 @@
+// var arr=['divya','vallabhapuram',21,5.5,true]
+// console.log(arr)
+// // arr.push('chitti')
+// // console.log(arr)
+// // arr.pop()
+// // console.log(arr)
+// // arr.shift()
+// // console.log(arr)
+// arr.unshift()
+// console.log(arr)
+// console.log(arr.length)
+// arr.indexOf(21)
+// console.log(arr)
+
+
+
